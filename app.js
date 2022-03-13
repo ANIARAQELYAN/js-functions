587cd8ba1e5ec27b036b361cd97e2f608ca6fa84
vache <vachaganaslanyan1992>
2022-03-13 21:55:56 +0400

    asdasdasd

---
+++ app.js
@@ -0,0 +1,35 @@
+// function addText(text,words){
+//     let nArr=text.split("");
+//     let item=0;
+//     for(let i=0;i<nArr.length;i++){
+//         if(nArr[i]=="_"){
+//             nArr[i]=words[item];
+//             item+=1
+//         }
+//     }
+//     return nArr.join('')
+// }
+// console.log(addText("_ we have a _", ["ann", "non problem"]));
+
+//Get('abc', ['bca', 'ab', 'bbccaa', 'bac']) return ['bca', 'bac'];
+//
+// function getWord(text, array) {
+//     let tt = text.split("");
+//     return tt
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] ===) {
+//
+//         }
+//     }
+// }
+//
+// console.log(getWord("ani"))
+//
+//
+// let arr = ["a", "n", "i"];
+// for (let i = 0; i < arr.length; i++) {
+//     arr.length[0] = arr.length - 1
+// }
+//
+//
+
